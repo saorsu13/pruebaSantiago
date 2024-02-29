c03dd9e0c574e14db14ac4497267821150d630fb
saorsu13 <sortizs@uqvirtual.edu.co>
2024-02-29 02:23:41 -0500

    Tercera entrega

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,8 @@ const usuariosRoutes = require('./routes/usuariosRoutes');
 const app = express();
 const port = 5000;
 
-const corsOptions = {
-  origin: 'https://prueba-santiago-jet.vercel.app',
-  optionsSuccessStatus: 200,
-  credentials: true
-};
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 // Usar las rutas de usuarios
